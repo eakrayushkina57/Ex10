b66a9c6c93ef29528a122bcc53798dcabed4d8c8
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2021-03-01 22:59:55 +0300

    b103

--- include/postfix.h
+++ include/postfix.h
@@ -2,7 +2,7 @@
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
 #include <string>
-#include <iostream>
+#include "MyStack.h"
 
 std::string infix2postfix(std::string infix);
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,5 @@
 #include "postfix.h"
 
 int main() {
-  std::string inf = "(6 * (4 - 2) + 5)*(2.6 + 3 * 7) - 9";
-  std::string postfix = infix2postfix(inf);
-  std::cout << postfix << std::endl;
   return 0;
 }
