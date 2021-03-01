4b536c77f3327fc67b2739f802ed72b4bf52827f
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2021-03-01 22:13:33 +0300

    b103

--- include/MyStack.h
+++ include/MyStack.h
@@ -6,83 +6,83 @@
 #include <string>
 #include <cassert>
 
-template<class S>
+template<class T>
 class MyStack {
  public:
   explicit MyStack(int size);
   ~MyStack();
-  MyStack(const MyStack<S>&);
+  MyStack(const MyStack<T>&);
   int getMyStackSize() const;
   int getUpper() const;
-  S* getPtr() const;
-  void push(const S&);
+  T* getPtr() const;
+  void push(const T&);
   void pop();
   void printMyStack();
-  S isEmpty() const;
-  S isFull() const;
-  S get() const;
+  T isEmpty() const;
+  T isFull() const;
+  T get() const;
  private:
   int size;
   int upper;
-  S* store;
+  T* store;
 };
 
-template<class S>
-MyStack<S>::MyStack(int _size) :
+template<class T>
+MyStack<T>::MyStack(int _size) :
   size(_size), upper(0) {
-  store = new S[size]; }
+  store = new T[size]; }
 
-template<class S>
-MyStack<S>::MyStack(const MyStack <S>& otherStack) :
+template<class T>
+MyStack<T>::MyStack(const MyStack <T>& otherStack) :
   size(otherStack.getMyStackSize()) {
-  store = new S[size];
+  store = new T[size];
   upper = otherStack.getUpper;
   for (int ix = 0; ix < upper; ix++)
     store[ix] = otherStack.getPtr()[ix]; }
 
-template<class S>
-MyStack<S>::~MyStack() {
+template<class T>
+MyStack<T>::~MyStack() {
   delete[] store; }
 
-template <class S>
-int MyStack<S>::getMyStackSize() const {
+template <class T>
+int MyStack<T>::getMyStackSize() const {
   return size; }
 
-template <class S>
-int MyStack<S>::getUpper() const {
+template <class T>
+int MyStack<T>::getUpper() const {
   return upper; }
 
-template <class S>
-S* MyStack<S>::getPtr() const {
+template <class T>
+S* MyStack<T>::getPtr() const {
   return store; }
 
-template <typename S>
-void MyStack<S>::push(const S& value) {
+template <typename T>
+void MyStack<T>::push(const T& value) {
   assert(upper < size);
   store[upper++] = value; }
 
-template <typename S>
-void MyStack<S>::pop() {
+template <typename T>
+void MyStack<T>::pop() {
   assert(upper > 0);
   store[--upper]; }
 
-template <typename S>
-void MyStack<S>::printMyStack() {
+template <typename T>
+void MyStack<T>::printMyStack() {
   for (int ix = upper - 1; ix >= 0; ix--)
     std:: cout << store[ix] << " "; }
 
-template <typename S>
-S MyStack<S>::isEmpty() const {
+template <typename T>
+S MyStack<T>::isEmpty() const {
   if (upper == 0) return 1;
   return 0; }
 
-template <typename S>
-S MyStack<S>::isFull() const {
+template <typename T>
+S MyStack<T>::isFull() const {
   if (upper == size) return 1;
   return 0; }
 
-template <typename S>
-S MyStack<S>::get() const {
+template <typename T>
+S MyStack<T>::get() const {
   if (upper != 0)
   return store[upper - 1]; }
 
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -7,59 +7,39 @@
 
 std::string infix2postfix(std::string infix) {
   std::string postfix;
-  std::unordered_map<char, int> return_prior({ {'(', 0}, {')', 1}, {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3} });
+  std::unordered_map<char, int> return_prior({ {'(', 0}, {')', 1},
+   {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3} });
   int count = 0;
   int l = inf.length();
   MyStack<char> operation(l+100);
-  for (count = 0; count < l; count++) 
-  {
-    if (inf[count] != ' ') 
-	{
-      if (inf[count] == '(') 
-	  {
+  for (count = 0; count < l; count++) {
+    if (inf[count] != ' ') {
+      if (inf[count] == '(') {
         operation.push(inf[count]);
-      } 
-	  else if (inf[count] == ')') 
-	  {
-        while (operation.get() != '(') 
-		{
+      } else if (inf[count] == ')') {
+        while (operation.get() != '(') {
           postfix += operation.pop();
-          postfix += ' '; 
-		}
+          postfix += ' '; }
         operation.pop();
-      } 
-	  else if (inf[count] == '+' || inf[count] == '-') 
-	  {
-        while (return_prior[operation.get()] > 1) 
-		{
+      } else if (inf[count] == '+' || inf[count] == '-') {
+        while (return_prior[operation.get()] > 1) {
           postfix += operation.pop();
-          postfix += ' '; 
-		}
+          postfix += ' '; }
         operation.push(inf[count]);
-      } 
-	  else if (inf[count] == '*' || inf[count] == '/') 
-	  {
-        while (operation.get() == '*' || operation.get() == '/') 
-		{
+      } else if (inf[count] == '*' || inf[count] == '/') {
+        while (operation.get() == '*' || operation.get() == '/') {
           postfix += operation.pop();
-          postfix += ' '; 
-		}
+          postfix += ' '; }
         operation.push(inf[count]);
-      } 
-	  else if (inf[count] >= '0' && inf[count] <= '9') 
-	  {
+      } else if (inf[count] >= '0' && inf[count] <= '9') {
         while (count < l && inf[count] != ')' &&
           inf[count] != ' ') { postfix += inf[count];
           count += 1; }
           count -= 1;
-          postfix += ' '; 
-	  }
+          postfix += ' '; }
     }
   }
   if (postfix.back() == ' ') postfix.pop_back();
-  while (!operation.isEmpty()) 
-  { 
-	postfix += ' ';
-    postfix += operation.pop(); 
-  }
+  while (!operation.isEmpty()) { postfix += ' ';
+    postfix += operation.pop(); }
   return postfix; }
